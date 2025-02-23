e27ea4007d88146b013ee485f97814597751a63f
Francisco Cardozo <fcardozo199@gmailcom>
2025-02-23 10:11:14 -0500

    fix controller

--- src/controllers/cinemaController.ts
+++ src/controllers/cinemaController.ts
@@ -6,7 +6,7 @@ const cinemaController = Router();
 const validator = OpenApiValidatorProvider.getValidator();
 const routeToValidate = 'v1/product/cinema';
 
-cinemaController.get('/movies/retrieve',
+cinemaController.get('/v1/product/cinema/movies/retrieve',
  async (req: Request, res: Response) => {
     CinemaService.getMovies(req, res); 
 });
