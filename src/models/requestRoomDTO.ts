dde6b83f5e896a8d67412ab256eeadb18d91ae1d
Francisco Cardozo <fcardozo199@gmailcom>
2025-02-23 16:51:47 -0500

    change rommDTO

--- src/models/requestRoomDTO.ts
+++ src/models/requestRoomDTO.ts
@@ -11,13 +11,13 @@ export default class RequestRoomDTO {
         this.room_id = request.roomId;
         this.room_capacity = request.roomCapacity;
         this.room_name = request.roomName;
-        this.room_reservations = request.roomReservation;
+        this.room_reservations = request.roomReservations;
     }
 
     public getDynamoObject(){
         const dynamoObject = {
             'room_id': { S: this.room_id },
-            'room_capacity': { N: this.room_capacity.toString() },
+            'room_capacity': { N: this.room_capacity },
             'room_name': { S: this.room_name },
             'room_reservations': { L: this.room_reservations }
         };
