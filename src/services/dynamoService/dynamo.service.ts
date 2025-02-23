27fdeaa2482b0dbbdc0bc37e00fd1e65dc6f1cb2
Francisco Cardozo <fcardozo199@gmailcom>
2025-02-23 10:44:41 -0500

    fix dynamo servce

--- src/services/dynamoService/dynamo.service.ts
+++ src/services/dynamoService/dynamo.service.ts
@@ -3,11 +3,15 @@ const dynamoDb = new DynamoDBClient({ region: 'us-east-1' });
 
 export default class DynamoService {
     public static async getItems(tableName: string, key?:string) {
-        const params = {
-            TableName: tableName,
-            Key: key ? {'PrimaryKey': key} : undefined
-        };
-        const command = new ScanCommand(params);
-        return dynamoDb.send(command).promise();
+        try {
+            const params = {
+                TableName: tableName,
+                Key: key ? {'PrimaryKey': key} : undefined
+            };
+            const command = new ScanCommand(params);
+            return dynamoDb.send(command);
+        } catch (e) {
+            console.log(e);
+        }
     }
 }
